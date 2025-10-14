69142cc39227d90c4de9f306d90d5eebd7c87a84
Suhas Palwai <suhaspalwai04@gmail.com>
2025-10-14 00:40:04 -0500

    init: creating initial neovim config

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,34 @@
+return {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+        require("mason-lspconfig").setup()
+
+        vim.diagnostic.config{
+            virtual_text = true,
+        }
+
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspKeymaps", {}),
+            callback = function(event)
+                local opts = { buffer = event.buf, silent = true }
+
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "<leader>f", function()
+                    vim.lsp.buf.format({ async = true })
+                end, opts)
+            end,
+        })
+    end
+}
