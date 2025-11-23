01eaa64a7330d197558c58a17392ebc9f597c4e4
Suhas Palwai <suhaspalwai04@gmail.com>
2025-11-23 10:17:03 -0600

    feat: adding harpoon plugin

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,7 +1,12 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.g.editorconfig = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
